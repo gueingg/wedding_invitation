34dd4983085fce612c6221e2a20f18c19c9be6b9
PC\hjpk2 <hjpk21@nate.com>
2025-06-21 19:50:12 +0900

    ci setting

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,12 @@
-import type { NextConfig } from "next";
+import type { NextConfig } from 'next';
+
+const isProd = process.env.NODE_ENV === 'production';
 
 const nextConfig: NextConfig = {
   /* config options here */
+  output: 'export',
+  assetPrefix: isProd ? '/wedding_invitation' : '',
+  basePath: isProd ? '/wedding_invitation' : '',
 };
 
 export default nextConfig;
