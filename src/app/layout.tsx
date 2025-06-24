a1c193e03e14e7156fd53dd118177ad707c86f1b
PC\hjpk2 <hjpk21@nate.com>
2025-06-24 18:26:31 +0900

    fix: viewport instead

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,8 +3,13 @@ import './globals.css';
 export const metadata = {
   title: 'Wedding Invitation',
   description: '박현규 & 신유진 결혼식에 초대합니다.',
-  viewport:
-    'width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no',
+};
+
+export const viewport = {
+  width: 'device-width',
+  initialScale: 1.0,
+  maximumScale: 1.0,
+  userScalable: 'no',
 };
 
 export default function RootLayout({
